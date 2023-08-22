2acd549289eaae01fa687af4b1f89b1aee7a8476
Brent Buchholz <brentbuchholz@gmail.com>
2023-08-22 14:57:37 -0500

    Add card to BUILDER

---
+++ src/pages/myrKiSs/BUILDER.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="center">
+    <p>
+      We are chasing a scent here, it has been going on
+      for over a decade now, and has led further than WE
+      could ever have hoped at outset of journey, so WE
+      continue, following where inspiration takes us,
+      trusting that each brick layed is another piece
+      of foundation fundamental...
+    </p>
+    <iframe
+      width="420" height="315"
+      src="https://www.youtube.com/embed/qAkZT_4vL_Y?playlist=qAkZT_4vL_Y&loop=1"
+    />
+    <p>
+      Some folks just gotta follow their noses, not quite
+      sure what they buildin, even as they build it. Same as it
+      ever was, if'n ya ask me, that's what leads some to
+      this particular corner of the PsyberGloam, that longing,
+      that search, deep down inside... - Jacob
+    </p>
+  </div>
+  <div>
+    <!-- <a href="https://anxious-grove.netlify.app/" target="_blank"> -->
+    <img class="card" src="/BUILDER-2f53.png">
+    <!-- </a> -->
+  </div>
+</template>
+
+<style>
+  .center {
+    text-align: center;
+  }
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
